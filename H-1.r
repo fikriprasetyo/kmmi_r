dbd8fdb37470d90162f05aa164981e2d2349c510
fikriprasetyo <fikriprasetyo17@gmail.com>
2021-08-11 10:16:08 +0700

    ya

--- H-1.r
+++ H-1.r
@@ -2,3 +2,21 @@
 2*3
 sqrt(36)
 log10(100)
+
+#tanda penulisan
+sebuah_data <- 20 # assign 20 to 'a'
+sebuah_data = 20
+20 -> sebuah_data # assign 20 to 'a'
+20 = sebuah_data
+
+#package dan library
+library(paket)
+print(datanya)
+data pertama = 1+1
+datapertama = 1 + 1
+print(datapertama)
+
+install.packages("nama package") # string
+install.packages(c("pakg1", "pakg2")) # string
+c("pakg1", "pakg2")
+length(c("pakg1", "pakg2")) == 2
